08050e1511f85baf8a8b7cc70fdb915c2dec9cb5
Lucky <lucky123@gmail.com>
2025-02-05 18:57:03 +0530

    middleware setup

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,18 @@
+ "use client"
+ 
+ import { Button } from '@/components/ui/button'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
 import React from 'react'
 
 function Dashboard() {
   return (
     <div>
       dashboard
+      <Button className='rounded-xl font-bold'>
+        <LogoutLink>
+LogOut
+        </LogoutLink>
+        </Button>
     </div>
   )
 }
