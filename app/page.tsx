228498f8330cf629697c0fa8c2e08bec8635feb0
Lucky <lucky123@gmail.com>
2025-02-05 18:41:23 +0530

    completed auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
  "use client"
+import { useEffect } from "react";
  import Footer from "./_components/Footer";
-// import Header from "./_components/Header";
+
 import Hero from "./_components/Hero";
 import { Navbar } from "./_components/Navbar";
-
+import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
 
 export default function Home() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const {user } = useKindeBrowserClient();
+
+  useEffect(()=>{
+    console.log(user)
+  },[user])
   return (
     <div>
-      {/* <Header/> */}
+      
       <Navbar/>
 
       <Hero/>
